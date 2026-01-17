ac696c96fca471b16458a213d2318a730f444094
Juniorleriche <juniorleriche27@users.noreply.github.com>
2026-01-17 15:58:47 +0000

    Force full-bleed layout for data-analyst pages

--- apps/koryxa/frontend/app/school/data-analyst/layout.tsx
+++ apps/koryxa/frontend/app/school/data-analyst/layout.tsx
@@ -5,10 +5,12 @@ import { DATA_ANALYST_MODULES } from "./data";
 
 export default function DataAnalystLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-0 flex-col gap-6 lg:flex-row">
-      <DataAnalystViewport />
-      <DataAnalystSidebar modules={DATA_ANALYST_MODULES} />
-      <div className="min-w-0 flex-1">{children}</div>
+    <div className="relative left-1/2 right-1/2 -mx-[50vw] w-screen overflow-x-hidden px-4 sm:px-6 lg:px-8">
+      <div className="flex min-h-0 flex-col gap-6 lg:flex-row">
+        <DataAnalystViewport />
+        <DataAnalystSidebar modules={DATA_ANALYST_MODULES} />
+        <div className="min-w-0 flex-1">{children}</div>
+      </div>
     </div>
   );
 }

ed7464a5232705c10d31f27d9875ab46a09e7ba0
Juniorleriche <juniorleriche27@users.noreply.github.com>
2026-01-17 16:26:10 +0000

    Make school sidebar dropdowns and independent scroll

--- apps/koryxa/frontend/app/school/data-analyst/layout.tsx
+++ apps/koryxa/frontend/app/school/data-analyst/layout.tsx
@@ -5,11 +5,15 @@ import { DATA_ANALYST_MODULES } from "./data";
 
 export default function DataAnalystLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="relative left-1/2 right-1/2 -mx-[50vw] w-screen overflow-x-hidden px-4 sm:px-6 lg:px-8">
-      <div className="flex min-h-0 flex-col gap-6 lg:flex-row">
-        <DataAnalystViewport />
-        <DataAnalystSidebar modules={DATA_ANALYST_MODULES} />
-        <div className="min-w-0 flex-1">{children}</div>
+    <div className="relative left-1/2 right-1/2 -mx-[50vw] h-full w-screen overflow-hidden px-4 sm:px-6 lg:px-8">
+      <DataAnalystViewport />
+      <div className="grid h-full min-h-0 grid-cols-1 gap-6 lg:grid-cols-[320px_minmax(0,1fr)]">
+        <div className="min-h-0 overflow-y-auto overscroll-contain">
+          <DataAnalystSidebar modules={DATA_ANALYST_MODULES} />
+        </div>
+        <div className="min-h-0 overflow-y-auto overscroll-contain pb-10 pt-6">
+          {children}
+        </div>
       </div>
     </div>
   );
